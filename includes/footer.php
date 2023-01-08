da810287661d680773eec4985d2dbd6b981037c7
gaweshab <89149747+gaweshab@users.noreply.github.com>
2023-01-08 22:11:43 +0530

    changes

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<div class="footer-bot">
+  <div class="container">
+    <div class="logo2">
+      <h2><a href="index.php"><span>Hood Lock</span> Manufacturing System</a></h2>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+</div>
+<div class="copy-right">
+  <div class="agileinfo_social_icons">
+    <ul class="agileits_social_list">
+      <li><a href="#" class="w3_agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href="#" class="w3_agile_dribble"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+      <li><a href="#" class="w3_agile_google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+    </ul>
+  </div>
+  <div class="container">
+    <p> © 2023 Copyright by Hood Lock Manufacturing Company. All Rights Reserved.</p>
+  </div>
+</div>
+<!-- //footer -->
---
+++ includes/header.php
@@ -0,0 +1,54 @@
+<!-- navigation -->
+<div class="nav-links">
+  <div class="header-w3l">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <h1><span>Hood Lock</span> Manufacturing System</h1>
+      </a>
+      <div class="call-w3l">
+        <img src="images1/call.png">
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+  <nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <div class="w3l-nav">
+        <ul class="nav navbar-nav link-effect">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="#about" class="scroll">About Us</a></li>
+          <li><a href="#contact" class="scroll">Contact Us</a></li>
+          <li><a href="#contact" class="scroll">Search Locker</a></li>
+          <li><a href="Main/index.php">Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="clearfix"></div>
+</div>
+<!-- //navigation -->
+<!-- banner section -->
+<div class="banner-slider">
+  <div class="container">
+    <!--Slider-->
+    <div class="callbacks_container">
+      <ul class="rslides" id="slider3">
+        <li>
+          <div class="w3l_banner_info">
+            <h3>Hood Lock Manufacturing System</h3>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <!-- //Slider -->
+
+  </div>
+</div>
