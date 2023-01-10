573e013e5f0c69339d1746a032c7d29a46fcfd27
DinanDias <93309200+DinanDias@users.noreply.github.com>
2023-01-10 12:20:24 +0530

    change in kanban css

--- Main/kanban.php
+++ Main/kanban.php
@@ -9,7 +9,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body style="padding:50px; padding-rihgt: 50px;">
+<body style="padding:50px;">
 	<center>
 		<br> <br>
 	<button onclick="window.location.href='./dashboard.php';" style="font-size:24px; float:left;">Back <i style="color: red;" class="fa fa-hand-o-left"></i></button> <br> <br> <br>
